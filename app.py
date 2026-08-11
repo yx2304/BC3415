23c95a4799ef8be7d82ab69281d0753e15c6ca4f
yx2304 <yuxyn.tan@gmail.com>
2026-08-11 08:09:04 +0000

    1

--- app.py
+++ app.py
@@ -1,1 +1,12 @@
 #dbs prediction
+
+from flask import Flask, render_template, request
+
+app = Flask(__name__) 
+
+@app.route("/", methods = ["GET", "POST"])
+def index():
+    return ("hi")
+
+if __name__ == "__main__":
+    app.run()
